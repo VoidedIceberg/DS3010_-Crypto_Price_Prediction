64beb1297bf6b7ea30e6b4c7504fdd0c03aed3b7
Troy Mullenberg <tmullenberg@hotmail.com>
2021-05-05 22:36:47 -0400

    Built out functionality of the CryptoData object using an API found for python!

--- CryptoData.py
+++ CryptoData.py
@@ -1,8 +1,34 @@
 # This class is an object to collect data on a periodic timer based on the passed in parameter
+#  %%
+import cryptocompare
+import datetime
+import json
+
 class CryptoData:
-    def __init__(self, tickerToCollect):
-        super(CryptoData, self).__init__(*args))
-    
+    def __init__(self):
+        # self.api_key = None
+        self.client = None
+        
     def authenticate(self):
-        pass;
+        with open('TwitterAuth.json') as f:
+            tokenFile = json.load(f)
+        api = cryptocompare.cryptocompare._set_api_key_parameter(tokenFile['CryptoDataKey'])
+        coinList = cryptocompare.get_coin_list(format=False)
+        print(coinList)
+    def getCurrentPrice(self, ticker):
+        return cryptocompare.get_price(ticker, currency='USD', full=False)
+    def getPastPrice(self, ticker):
+            return cryptocompare.get_historical_price(ticker, 'USD', datetime.datetime(2017,6,6))
+    def getPastPriceHour(self, ticker):
+        return cryptocompare.get_historical_price_hour(ticker, currency='USD')
+
     
+# %%
+
+CD = CryptoData()
+
+CD.authenticate()
+print(CD.getCurrentPrice('BTC'))
+print(CD.getPastPrice('BTC'))
+time = CD.getPastPriceHour("BTC")
+# %%
