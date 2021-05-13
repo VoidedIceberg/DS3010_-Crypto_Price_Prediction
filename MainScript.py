c88f149590c8bca030ece2c0cf33d19d0f887102
Troy Mullenberg <tmullenberg@hotmail.com>
2021-05-13 02:13:36 -0400

    Cleaning up formating and undoing the interactive notebook style for a function based approch to try to leverage sklearns multithreading capibilites better.

--- MainScript.py
+++ MainScript.py
@@ -1,4 +1,3 @@
-#  %%
 #  This file is to use the other objects and thread them to allow simutanious collection
 import pymongo
 import json
@@ -9,13 +8,107 @@ from sklearn.model_selection._split import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+
+
 
 # Fetches the Month histroy and pushes it into mongoDB
 def getBTCHistoricalData():
     histroy = CD.getPastPriceHour("BTC") 
     coll = cryptoDB['BTC_History']
     _ = coll.insert_many(histroy)
+
+def putInDict():
+    last = None
+    for hour in cryptoDB['BTC_History'].find().sort('time', -1):
+        if (last != None):
+            upOrdown = hour['close'] - last['close']
+            if (upOrdown > 0 ):
+                print("up")
+                for tweet in cryptoDB['Bitcoin'].find({'created_at' : { '$gt' : hour['time'], '$lt' :  last['time']}}):
+                    data['mvmt'].append("up")
+                    data['tweet'].append(tweet['text'])
+                    print(tweet['text'])
+            else:
+                print("down")
+                for tweet in cryptoDB['Bitcoin'].find({'created_at' : { '$gt' : hour['time'], '$lt' :  last['time']}}):
+                    data['mvmt'].append("down")
+                    data['tweet'].append(tweet['text'])
+                    print(tweet['text'])
+        last = hour
+def runTheLinearSVC():
+    docs_train, docs_test, y_train, y_test = train_test_split( data['tweet'], data['mvmt'], test_size=0.25, random_state=None )
+
+    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
+    pipeline = Pipeline([
+            ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+            ('clf', LinearSVC(C=1000)),
+        ])
+
+        # TASK: Build a grid search to find out whether unigrams or bigrams are
+        # more useful.
+        # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+            'vect__ngram_range': [(1, 1), (1, 2)],
+        }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    grid_search.fit(docs_train, y_train)
     
+    # TASK: print the mean and std for each candidate along with the parameter
+    # settings for all the candidates explored by grid search.
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (grid_search.cv_results_['params'][i],
+                    grid_search.cv_results_['mean_test_score'][i],
+                    grid_search.cv_results_['std_test_score'][i]))
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    y_predicted = grid_search.predict(docs_test)
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted))
+    # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+    plt.matshow(cm)
+    plt.show()
+def runKNN():
+    docs_train, docs_test, y_train, y_test = train_test_split( data['tweet'], data['mvmt'], test_size=0.25, random_state=None )
+
+    pipeline = Pipeline([
+    ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+    ('clf', KNeighborsClassifier(n_neighbors=3)),
+    ])
+
+    # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+    }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    grid_search.fit(docs_train, y_train)
+
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (grid_search.cv_results_['params'][i],
+                    grid_search.cv_results_['mean_test_score'][i],
+                    grid_search.cv_results_['std_test_score'][i]))
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    y_predicted = grid_search.predict(docs_test)
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted))
+    # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+    plt.matshow(cm)
+    plt.show()
+    
+    
+        
 if __name__ == '__main__':
     with open('TwitterAuth.json') as f:
         tokenFile = json.load(f)
@@ -41,81 +134,9 @@ if __name__ == '__main__':
     #     lasthour = hour['time']
         
     data = {'tweet' : [], 'mvmt' : []}
-    last = None
-    # %%
-    for hour in cryptoDB['BTC_History'].find().sort('time', -1):
-        if (last != None):
-            upOrdown = hour['close'] - last['close']
-            if (upOrdown > 0 ):
-                print("up")
-                for tweet in cryptoDB['Bitcoin'].find({'created_at' : { '$gt' : hour['time'], '$lt' :  last['time']}}):
-                    data['mvmt'].append("up")
-                    data['tweet'].append(tweet['text'])
-                    print(tweet['text'])
-            else:
-                print("down")
-                for tweet in cryptoDB['Bitcoin'].find({'created_at' : { '$gt' : hour['time'], '$lt' :  last['time']}}):
-                    data['mvmt'].append("down")
-                    data['tweet'].append(tweet['text'])
-                    print(tweet['text'])
-        last = hour
-        # %%
-        print(len(data['tweet']))
-        print(len(data['mvmt']))
-
-        
-        # %%
-            # split the dataset in training and test set:
-docs_train, docs_test, y_train, y_test = train_test_split( data['tweet'], data['mvmt'], test_size=0.25, random_state=None )
-
-    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
-    # that are too rare or too frequent
-pipeline = Pipeline([
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        ('clf', LinearSVC(C=1000)),
-    ])
-
-    # TASK: Build a grid search to find out whether unigrams or bigrams are
-    # more useful.
-    # Fit the pipeline on the training set using grid search for the parameters
-parameters = {
-        'vect__ngram_range': [(1, 1), (1, 2)],
-    }
-grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-grid_search.fit(docs_train, y_train)
-# %%
-from sklearn import metrics
-
-# TASK: print the mean and std for each candidate along with the parameter
-# settings for all the candidates explored by grid search.
-n_candidates = len(grid_search.cv_results_['params'])
-for i in range(n_candidates):
-    print(i, 'params - %s; mean - %0.2f; std - %0.2f'
-             % (grid_search.cv_results_['params'][i],
-                grid_search.cv_results_['mean_test_score'][i],
-                grid_search.cv_results_['std_test_score'][i]))
-# TASK: Predict the outcome on the testing set and store it in a variable
-# named y_predicted
-y_predicted = grid_search.predict(docs_test)
-# Print the classification report
-print(metrics.classification_report(y_test, y_predicted))
-# Print and plot the confusion matrix
-cm = metrics.confusion_matrix(y_test, y_predicted)
-print(cm)
-
-import matplotlib.pyplot as plt
-plt.matshow(cm)
-plt.show()
-        
-            
-
-
-        
-
-
-
-
- 
-        
-
-# %%
+    putInDict()
+    print(len(data['tweet']))
+    print(len(data['mvmt']))
+    # runTheLinearSVC()
+    runKNN()
+       
